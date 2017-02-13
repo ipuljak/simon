4ebf91b35071b11e31539f99cd3e38debc69b004
Ivan <ivanpulj@gmail.com>
2017-02-13 01:15:40 -0500

    Added README file and some code documentation

---
+++ README.md
@@ -0,0 +1,15 @@
+#Simon Game
+
+An attempt to recreate the famous [Simon game](https://en.wikipedia.org/wiki/Simon_(game)) of the 1980's written in Javascript with jQuery & HTML/CSS. Upon starting it, the game will begin to display a series of flashing lights and sounds which the user must replicate, with the series incrementing by one at each successful stage. Implemented to gain a better understanding of Javascript timers.
+
+###How To###
+
+To play the Simon game, clone the repo and open the index.html file in a browser of your choice.
+
+###Live Demo###
+
+You can try out the live demo [here](https://www.puljak.ca/projects/basic/simon).
+
+####About Me#####
+
+For more information about me, visit my website at [puljak.ca](https://puljak.ca)!
--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-  // Initial bools
+  // Instantiate initial boolean values
   var on = false;
   var strict = false;
   var lock = true;
   var userPushed = false;
 
-  // Game object
+  // Create the main game object
   var game = {};
-  game.init = function() {
+  game.init = function () {
     this.play;
     this.lock;
     this.limit = 10;
@@ -20,7 +20,7 @@ $(document).ready(function() {
     this.done = false;
   };
 
-  // Corners and lights
+  // Define the game lights
   var lights = {
     "#topLeft": "green-light",
     "#topRight": "red-light",
@@ -28,10 +28,11 @@ $(document).ready(function() {
     "#bottomRight": "blue-light"
   };
 
+  // Define the corner game buttons
   var corners = ["#topLeft", "#topRight", "#bottomLeft", "#bottomRight"];
   var lightOns = ['green-light', 'red-light', 'yellow-light', 'blue-light'];
 
-  // Sounds
+  // Define the game sounds
   var green = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
   var red = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3");
   var yellow = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3");
@@ -39,62 +40,67 @@ $(document).ready(function() {
 
   var sounds = [green, red, yellow, blue];
 
-  // On switch
-  $("#onSwitch").on("click", function() {
+  // Click event to toggle the game on and off
+  $("#onSwitch").on("click", function () {
     toggleOn();
   });
 
-  // Start button
-  $("#start").on("click", function() {
+  // Click event to start the game if it is turned on
+  $("#start").on("click", function () {
     if (on) {
       startGame();
     }
   });
 
-  // Strict button
-  $("#strict").on("click", function() {
+  // Click event to set the "strict" game mode if it is turned on
+  $("#strict").on("click", function () {
     if (on) {
       toggleStrict();
     }
   });
 
-  // Corner mousedown
-  $(".corner").on("mousedown", function() {
+  // Mousedown event for when the user clicks a corner button
+  $(".corner").on("mousedown", function () {
+    // If the game is not in a locked mode
     if (!lock) {
+      // Play the color and turn it's light on
       $(this).toggleClass(lights["#" + this.id]);
       sounds[corners.indexOf("#" + this.id)].play();
-
+      // let the game logic know that the user depressed an option
       game.userSelect = "#" + this.id;
       userPushed = true;
     }
   });
 
-  // Body mouseup
-  $("body").on("mouseup", function() {
+  // Mouseup event for then user releases their mouseup
+  // It's necessary to put a mouseup for the entire body as the user may move their mouse away from a button while they click it
+  $("body").on("mouseup", function () {
+    // If the game is not locked and the user has depressed an option
     if (!lock && userPushed) {
+      // Kill all of the lights and let the game know the click event is finished
       setTimeout(lightsOff, 500);
       userPushed = false;
       pushed();
     }
   });
 
-  // Shut off all of the lights
+  // Shut off all of the corner lights
   function lightsOff() {
     for (var i = 0; i < lightOns.length; i++) {
-      $(".corner").each(function() {
+      $(".corner").each(function () {
         $(this).removeClass(lightOns[i]);
       });
     }
   }
 
-  // Turn on all of the lights
+  // Turn on all of the corner lights
   function lightsOn() {
     for (var i = 0; i < corners.length; i++) {
       $(corners[i]).addClass(lightOns[i]);
     }
   }
 
-  // Kill all of the timers and lights, and set the locks
+  // Kill all of the game timers and lights, and set the locks
   function kill() {
     lightsOff();
     lock = true;
@@ -102,18 +108,18 @@ $(document).ready(function() {
     clearTimeout(game.lock);
     $(".congrats").addClass("hidden");
     $("#counterText").text("--");
-    $('.corner').each(function() {
+    $('.corner').each(function () {
       $(this).removeClass("clickable");
     });
   }
 
-  // Toggle the strict function
+  // Toggle the strict functionality
   function toggleStrict() {
     strict = !strict;
     $("#strictLight").toggleClass("onLight");
   }
 
-  // Turn the game on/off
+  // Turn the game on and off
   function toggleOn() {
     if (on) {
       if (strict) {
@@ -139,7 +145,7 @@ $(document).ready(function() {
     sequence();
   }
 
-  // Generate a random sequence pattern
+  // Generate a random sequence pattern from the four buttons
   function generateRandom() {
     var pattern = [];
     for (var i = 0; i < 20; i++) {
@@ -151,14 +157,17 @@ $(document).ready(function() {
 
   // Play the current game sequence
   function sequence() {
-    $('.corner').each(function() {
+    // While the game is playing the sequence, do not let the user click the buttons
+    $('.corner').each(function () {
       $(this).removeClass("clickable");
     });
 
+    // Set the user's counter and lock the game
     $("#counterText").text(game.index);
     lock = true;
+    // Start from the first part of the sequence and begin to play each step
     game.curPattern = 0;
-    game.play = setInterval(function() {
+    game.play = setInterval(function () {
       play();
       if (game.curPattern + 1 === game.index) {
         game.curPattern = 0;
@@ -167,6 +176,7 @@ $(document).ready(function() {
       game.curPattern++;
     }, 1200);
 
+    // Set locks so that the user cannot fiddle with anything while the game is playing
     game.userInd = 0;
     game.lock = setTimeout(gameLock, 1200 * (game.index + 1));
   }
@@ -180,7 +190,7 @@ $(document).ready(function() {
     $(corner).toggleClass(light);
     sounds[index].play();
 
-    setTimeout(function() {
+    setTimeout(function () {
       $(corner).toggleClass(light);
     }, 1000);
   }
@@ -189,7 +199,7 @@ $(document).ready(function() {
   function gameLock() {
     lock = false;
 
-    $('.corner').each(function() {
+    $('.corner').each(function () {
       $(this).addClass("clickable");
     });
   }
@@ -197,15 +207,12 @@ $(document).ready(function() {
   // User pushed a corner down
   function pushed() {
     var index = game.pattern[game.userInd];
-
-    // if the user guessed the current option correctly
+    // If the user guessed the current option correctly
     if (corners[index] === game.userSelect) {
       game.userInd++;
-
-      // if the user got the sequence correctly
+      // If the user got the sequence correctly
       if (game.userInd === game.index) {
-
-        // if the game is at the last stage, end and celebrate
+        // If the game is at the last stage, end and celebrate
         if (game.index === game.limit) {
           lock = true;
           game.done = true;
@@ -214,16 +221,13 @@ $(document).ready(function() {
           $("#counterText").text(":)");
           $(".congrats").removeClass("hidden");
           setTimeout(startGame, 5000);
-          // flash all
-
-          // if it's not at the last stage, raise the current index by one
+          // If it's not at the last stage, raise the current index by one
         } else {
           game.index++;
           sequence();
         }
       }
-
-      // user guessed incorrectly, replay sequence
+      // If the user guessed incorrectly, replay sequence
     } else {
       $("#counterText").text("!!!");
       if (strict) {
